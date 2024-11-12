e1c5bb40f4cfd247387a4291daf4ae6c02d7e118
Bahtiyorjon1997 <bahtiyorjonq777@gmail.com>
2024-11-12 13:50:26 +0100

    Type conversion and coercion

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+// type conversion - manual conversion
+const age = '12'
+console.log(typeof Number(age))
+
+// type coercion - types get converted automatically by the js under the hood
+console.log(12 + '21') // result -> "1221"
+
+console.log('12' - 21) // result -> -9
