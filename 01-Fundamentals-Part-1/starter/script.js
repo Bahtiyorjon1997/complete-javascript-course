db21d363adaf42932c2d1dc9f65223fb8578308c
Bahtiyorjon1997 <bahtiyorjonq777@gmail.com>
2024-11-12 17:25:04 +0100

    some usefull notes regarding js fundamentals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -6,3 +6,30 @@ console.log(typeof Number(age))
 console.log(12 + '21') // result -> "1221"
 
 console.log('12' - 21) // result -> -9
+
+// truthy and falsy values
+// there is 5 falsy values in JS: 0, "", null, undefined, NaN
+console.log("''", Boolean(''))
+console.log(0, Boolean(0))
+console.log('[]', Boolean([]))
+
+// loose and string equality
+18 == '18' // result -> true | loose equality
+18 === '18' // result -> false | strict equality
+
+// switch case statements
+const day = 'Monday'
+
+switch (day) {
+  case 'Monday':
+    console.log('Monday')
+    break
+  case 'Sunday':
+    console.log('Sunday')
+    break
+  default:
+    console.log('default')
+}
+
+//  backwards compatibility  - old ecma script version code (lets say 1997) is supported in the new browsers.
+// JS is backwards compatible because nothing has never been removed from the language, but only new stuff has been added.
