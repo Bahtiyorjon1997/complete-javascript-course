d803eb8141b28ba948e0488f3f09a5dab6ab63dd
Bahtiyorjon1997 <bahtiyorjonq777@gmail.com>
2024-11-13 10:53:03 +0100

    get started with arrays

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+'use strict' // strict mode activation
+
+// function
+function loggerGuy(log) {
+  console.log(`This is your boss, ${log}.`)
+}
+
+// invoking / running / calling function
+loggerGuy('Bakhtee')
+
+// arrays
+//  only primitive types are only not mutable
+
+const friendsArray = ['A', 'B', 'J', 'B']
+
+const friendsArray2 = new Array('A', 'B', 'J', 'B')
